59321cf8575900e346a703554210b2491244ab6d
Varvara Kronberg <github@varvara.ca>
2019-01-05 09:36:31 -0800

    practice

--- 02-array-sum.js
+++ 02-array-sum.js
@@ -4,6 +4,11 @@
 
 function sum(arr) {
     // your code here
+    var result = 0;
+    for (let i = 0; i < arr.length; i++) {
+        result += arr[i];
+    }
+    return result;
 }
 
 module.exports = sum;
